30e02d6df0f4980c8b34d549b7ceb9aad896fb5a
Deniz Kusefoglu <denizk@gmail.com>
2023-02-25 17:44:43 -0800

    set up updater page

--- pages/updater.js
+++ pages/updater.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
-const TimerDisplayComponent = dynamic(() => import('../components/TimerDisplayComponent'), { ssr: false });
+const TimerUpdaterComponent = dynamic(() => import('../components/TimerUpdaterComponent'), { ssr: false });
 
 export default function Home() {
   return (
     <div className="container">
       <main>
-        <TimerDisplayComponent />
+        <TimerUpdaterComponent />
       </main>
     </div>
   )
