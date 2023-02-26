6a6812be78a31928be2079ad8ac2f9ed480e560a
Deniz Kusefoglu <denizk@gmail.com>
2023-02-25 18:39:18 -0800

    updater works

--- components/TimerUpdaterComponent.jsx
+++ components/TimerUpdaterComponent.jsx
@@ -5,29 +5,18 @@ import styles from './AblyChatComponent.module.css';
 const AblyChatComponent = () => {
 
   let inputBox = null;
-  const [messageText, setMessageText] = useState("");
-  const [receivedMessages, setMessages] = useState([{connectionId:"me", data:"mpoo"}]);
-  const messageTextIsEmpty = messageText.trim().length === 0;
+  const [messageText, setMessageText] = useState("1");
+  const [receivedMessages, setMessages] = useState([{"data": "ready"}]);
 
   const [channel, ably] = useChannel("chat-demo", (message) => {
     console.log("received message")
-
-    // Here we're computing the state that'll be drawn into the message history
-    // We do that by slicing the last 199 messages from the receivedMessages buffer
-
-    const history = receivedMessages.slice(-199);
-    setMessages([...history, message]);
-
-    // Then finally, we take the message history, and combine it with the new message
-    // This means we'll always have up to 199 message + 1 new message, stored using the
-    // setMessages react useState hook
+    setMessages([message]);
   });
 
   const sendChatMessage = (messageText) => {
-    console.log("sendChatMessage")
+    console.log("sendChatMessage", messageText)
     channel.publish({ name: "chat-message", data: messageText });
-    setMessageText("");
-    inputBox.focus();
+    setMessageText("1");
   }
 
   const handleFormSubmission = (event) => {
@@ -37,39 +26,37 @@ const AblyChatComponent = () => {
     sendChatMessage(messageText);
   }
 
-  const handleKeyPress = (event) => {
-    console.log("handleKeyPress")
-    if (event.charCode !== 13 || messageTextIsEmpty) {
-      return;
-    }
-    sendChatMessage(messageText);
-    event.preventDefault();
-  }
-
   const messages = receivedMessages.map((message, index) => {
     // const author = message.connectionId === ably.connection.id ? "me" : "other";
     return <span key={index} className={styles.message} data-author={"me"}>{message.data}</span>;
   });
 
   return (
-    // <div className={styles.chatHolder}>
-      // <div className={styles.chatText}>
-      <div>
+    <div>
       <div>
         {messages}
-        <div ref={(element) => { messageEnd = element; }}></div>
       </div>
-      <form onSubmit={handleFormSubmission} className={styles.form}>
+
+      <div>
         <textarea
           ref={(element) => { inputBox = element; }}
           value={messageText}
-          placeholder="Type a message..."
           onChange={e => setMessageText(e.target.value)}
-          onKeyPress={handleKeyPress}
-          className={styles.textarea}
         ></textarea>
-        <button type="submit" className={styles.button} disabled={messageTextIsEmpty}>Send</button>
-      </form>
+      </div>
+
+      <button onClick={() => {
+        sendChatMessage(String(Number(messageText) * 60 * 60 * 1000))
+      }}>+1 Hour</button>
+      <button onClick={() => {
+        sendChatMessage(String(Number(messageText) * 24 * 60 * 60 * 1000))
+      }}>+1 Day</button>
+      <button onClick={() => {
+        sendChatMessage(String(Number(messageText) * -60 * 60 * 1000))
+      }}>-1 Hour</button>
+      <button onClick={() => {
+        sendChatMessage(String(Number(messageText) * -24 * 60 * 60 * 1000))
+      }}>-1 Day</button>
     </div>
   )
 }
